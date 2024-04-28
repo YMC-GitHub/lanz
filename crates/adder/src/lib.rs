b4d79ae35fffd864405f868d350c9f3ffdb402c4
yemiancheng <ymc.github@gmail.com>
2024-04-28 13:20:27 +0800

    docs(test): note for test

--- crates/adder/src/lib.rs
+++ crates/adder/src/lib.rs
@@ -8,7 +8,10 @@ mod tests {
 
     #[test]
     fn it_works() {
+        // this is a test func , please add #[test] above it.
         let result = add(2, 2);
-        assert_eq!(result, 4);
+        assert!(result == 4); // test bool
+        assert_eq!(result, 4); //test eq
+        assert_ne!(result, 8); //test ne
     }
 }
